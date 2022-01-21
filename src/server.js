6057557e46d1a7ae789c391cf6ec4cb8e2f91d84
Sharmee Biswas <sharmee.biswas@walmartlabs.com>
2022-01-21 19:23:22 +0530

    final release build

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import routes from "./routes/myBlogRoutes";
 import mongoose from "mongoose";
-import path from path;
+import path from "path";
 
 const app = express();
 const PORT = "8000";
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, "/build")));
 app.use(bodyParser.json());
 routes(app);
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/build/index.html'));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
 app.listen(PORT, () => console.log(`my-blog running on ${PORT}`));
